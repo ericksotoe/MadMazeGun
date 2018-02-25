32689905ca723a665161f6d247544bda7b2ca278
Erick E Soto <eehuerta@UCSD.EDU>
2018-02-24 20:26:13 -0800

    deleted unwanted assests, ready to change guns with diff ammo

---
+++ Assets/RedGunController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedGunController : MonoBehaviour {
+
+    public GameObject newMat;
+    public Texture green;
+    public Texture white;
+    public Texture blue;
+    public Texture red;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // do if statements and change color depednig on color pickup
+        newMat.GetComponent<MeshRenderer>().material.SetTexture("_EmmisionMap", green);
+		
+	}
+}
