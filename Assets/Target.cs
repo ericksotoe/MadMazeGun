349eb3b67ce07322d05d1146d961095fb3616a56
Erick E Soto <eehuerta@UCSD.EDU>
2018-02-24 19:01:17 -0800

    added functionality for gun in MadMaze

---
+++ Assets/Target.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    // health of the target
+    public float health = 50f;
+
+    public GameObject brokenCrate;
+    public void TakeDamage(float amount)
+    {
+        health -= amount; // decreases the health of the target
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+	
+
+    void Die()
+    {
+        Instantiate(brokenCrate, gameObject.transform.position, gameObject.transform.rotation);
+        Destroy(gameObject);
+        //Instantiate(brokenCrate);
+    }
+}
